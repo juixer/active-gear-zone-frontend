b9b50c9ceeaf7031232b99e71968070c25f5a3a4
Ashfakul Islam <r4pido3@gmail.com>
2024-07-09 01:30:17 +0600

    footer added

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,94 @@
+import {
+  FaFacebook,
+  FaInstagram,
+  FaTiktok,
+  FaXTwitter,
+  FaYoutube,
+} from "react-icons/fa6";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+const Footer = () => {
+
+    const date = new Date();
+
+    const getYear = date.getFullYear();
+
+
+  return (
+    <div className="bg-black text-white py-2">
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 lg:gap-10 max-w-screen-xl mx-auto px-3 py-5">
+        <div className="space-y-3">
+          <h2 className="text-xl font-medium uppercase">About Us</h2>
+          <p className="text-zinc-500 ">
+          Welcome to Active Gear Zone. We are dedicated to supporting athletes of all levels with the best products and outstanding customer service. Let's gear up and achieve greatness together!
+          </p>
+          <div className="flex gap-2 text-2xl">
+            <FaFacebook className="cursor-pointer hover:bg-lime-400 hover:text-black hover:border-none duration-300 border rounded-full p-2 text-4xl " />
+            <FaInstagram className="cursor-pointer hover:bg-lime-400 hover:text-black hover:border-none duration-300 border rounded-full p-2 text-4xl " />
+            <FaXTwitter className="cursor-pointer hover:bg-lime-400 hover:text-black hover:border-none duration-300 border rounded-full p-2 text-4xl " />
+            <FaYoutube className="cursor-pointer hover:bg-lime-400 hover:text-black hover:border-none duration-300 border rounded-full p-2 text-4xl " />
+            <FaTiktok className="cursor-pointer hover:bg-lime-400 hover:text-black hover:border-none duration-300 border rounded-full p-2 text-4xl " />
+          </div>
+        </div>
+
+        <div>
+          <h2 className="text-xl font-medium uppercase">Customer Service</h2>
+          <ul>
+            <li className=" mt-2 hover:text-white text-zinc-500 duration-300 cursor-pointer">
+              FAQ
+            </li>
+
+            <li className=" mt-2 hover:text-white text-zinc-500 duration-300 cursor-pointer">
+              Order Status
+            </li>
+            <li className=" mt-2 hover:text-white text-zinc-500 duration-300 cursor-pointer">
+              Returns
+            </li>
+            <li className=" mt-2 hover:text-white text-zinc-500 duration-300 cursor-pointer">
+              Shipping
+            </li>
+          </ul>
+        </div>
+
+        <div>
+          <h2 className="text-xl font-medium uppercase">Quick links</h2>
+          <ul>
+            <li className=" mt-2 hover:text-white text-zinc-500 duration-300 cursor-pointer">
+              Home
+            </li>
+
+            <li className=" mt-2 hover:text-white text-zinc-500 duration-300 cursor-pointer">
+              All Products
+            </li>
+            <li className=" mt-2 hover:text-white text-zinc-500 duration-300 cursor-pointer">
+              Manage Products
+            </li>
+            <li className=" mt-2 hover:text-white text-zinc-500 duration-300 cursor-pointer">
+              Cart
+            </li>
+            <li className=" mt-2 hover:text-white text-zinc-500 duration-300 cursor-pointer">
+              About Us
+            </li>
+          </ul>
+        </div>
+
+        <div className="space-y-3">
+          <h2 className="text-xl font-medium uppercase">let's get in touch</h2>
+          <p className="text-zinc-500 text-justify max-w-64">
+            Sign up for get updates and new hot deals
+          </p>
+          <div className="flex w-full max-w-sm items-center space-x-2">
+            <Input className="text-black " type="email" placeholder="Email" />
+            <Button className="bg-lime-400 text-black hover:bg-lime-600 duration-300 " type="submit">Sign Up</Button>
+          </div>
+        </div>
+      </div>
+        <hr className="border-zinc-800 mb-1"/>
+        <div className="flex justify-center items-center">
+            <p className="text-sm text-zinc-500">Copyright © {getYear} Active Gear Zone. All rights reserved.</p>
+        </div>
+    </div>
+  );
+};
+export default Footer;
--- src/components/layout/Root.tsx
+++ src/components/layout/Root.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../Footer/Footer";
 
 const Root = () => {
     return(
         <div>
-             <div className="max-w-screen-2xl mx-auto py-5">
+             <div className="max-w-screen-2xl min-h-screen mx-auto py-5 px-3">
                 <Outlet/>
              </div>
+             <Footer/>
         </div>
     )}
 export default Root;
