5e67edf6d9cb34cb3cd34a4857cb09ca14f0f781
Ashfakul Islam <r4pido3@gmail.com>
2024-07-09 16:35:14 +0600

    carousel refactored

--- src/components/Carousel/Slider.tsx
+++ src/components/Carousel/Slider.tsx
@@ -10,93 +10,54 @@ import { Button } from "../ui/button";
 import Autoplay from "embla-carousel-autoplay";
 
 const Slider = () => {
+  const items = [
+    {
+      img: "https://i.ibb.co/XDm9ZGT/pic-1-1.png",
+      text: "Unleash your Potential with Elite Gear",
+    },
+    {
+      img: "https://i.ibb.co/FsFRb6Y/pic-2-1.png",
+      text: "Unleash your Potential with Elite Gear",
+    },
+    {
+      img: "https://i.ibb.co/XDynBQr/pic-5.png",
+      text: "Unleash your Potential with Elite Gear",
+    },
+    {
+      img: "https://i.ibb.co/JC2F1bZ/pic-4-1.png",
+      text: "Unleash your Potential with Elite Gear",
+    },
+  ];
   return (
     <Carousel
       className="w-full"
       plugins={[
         Autoplay({
-          delay: 3000,
+          delay: 4000,
         }),
       ]}
       opts={{ align: "start", loop: true }}
     >
       <CarouselContent>
-        {/* pic 1 */}
-        <CarouselItem>
-          <div className="p-1 relative">
-            <div className="md:top-[35%] top-[10%] left-[10%] absolute">
-              <h1 className="md:text-5xl text-white  font-semibold max-w-48 md:max-w-96 mb-3">
-                Unleash your Potential with Elite Gear
-              </h1>
-              <NavLink to={"/products"}>
-                <Button className="bg-baseColor hover:bg-lime-600 duration-300 text-black">
-                  Discovery Now
-                </Button>
-              </NavLink>
+        {items.map((item, index) => (
+          <CarouselItem
+            key={index}
+          >
+            <div className="p-1 relative">
+              <div className="md:top-[35%] top-[10%] left-[10%] absolute">
+                <h1 className="md:text-5xl text-white font-semibold max-w-48 md:max-w-96 mb-3">
+                  {item.text}
+                </h1>
+                <NavLink to={"/products"}>
+                  <Button className="bg-baseColor hover:bg-lime-600 duration-300 text-black">
+                    Discover Now
+                  </Button>
+                </NavLink>
+              </div>
+              <img src={item.img} className="rounded-md" />
             </div>
-            <img
-              src="https://i.ibb.co/XDm9ZGT/pic-1-1.png"
-              className="rounded-md"
-            />
-          </div>
-        </CarouselItem>
-        {/* pic 2 */}
-        <CarouselItem>
-          <div className="p-1 relative">
-            <div className="md:top-[35%] top-[10%] left-[10%] absolute">
-              <h1 className="md:text-5xl text-white  font-semibold max-w-48 md:max-w-96 mb-3">
-                Unleash your Potential with Elite Gear
-              </h1>
-              <NavLink to={"/products"}>
-                <Button className="bg-baseColor hover:bg-lime-600 duration-300 text-black">
-                  Discovery Now
-                </Button>
-              </NavLink>
-            </div>
-            <img
-              src="https://i.ibb.co/FsFRb6Y/pic-2-1.png"
-              className="rounded-md"
-            />
-          </div>
-        </CarouselItem>
-        {/* pic 3 */}
-        <CarouselItem>
-          <div className="p-1 relative">
-            <div className="md:top-[35%] top-[10%] left-[10%] absolute">
-              <h1 className="md:text-5xl text-white  font-semibold max-w-48 md:max-w-96 mb-3">
-                Unleash your Potential with Elite Gear
-              </h1>
-              <NavLink to={"/products"}>
-                <Button className="bg-baseColor hover:bg-lime-600 duration-300 text-black">
-                  Discovery Now
-                </Button>
-              </NavLink>
-            </div>
-            <img
-              src="https://i.ibb.co/XDynBQr/pic-5.png"
-              className="rounded-md"
-            />
-          </div>
-        </CarouselItem>
-        {/* pic 4 */}
-        <CarouselItem>
-          <div className="p-1 relative">
-            <div className="md:top-[35%] top-[10%] left-[10%] absolute">
-              <h1 className="md:text-5xl text-white  font-semibold max-w-48 md:max-w-96 mb-3">
-                Unleash your Potential with Elite Gear
-              </h1>
-              <NavLink to={"/products"}>
-                <Button className="bg-baseColor hover:bg-lime-600 duration-300 text-black">
-                  Discovery Now
-                </Button>
-              </NavLink>
-            </div>
-            <img
-              src="https://i.ibb.co/JC2F1bZ/pic-4-1.png"
-              className="rounded-md"
-            />
-          </div>
-        </CarouselItem>
+          </CarouselItem>
+        ))}
       </CarouselContent>
       <CarouselPrevious />
       <CarouselNext />
